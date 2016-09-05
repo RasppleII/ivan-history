a72fe9167b5fe26b1f6f266a04b1a973a3aa9e58
T. Joseph Carter <tjcarter@spiritsubstance.com>
2016-09-05 16:38:27 -0700

    bash_history-*: Document the history "commands"
    This will be the last commit where the "commands" from Ivan's bash
    history files are maintained in any way.  Before doing away with them,
    it seemed only appropriate to document what they did exactly.
    
    Also took a moment to translate the sed command into English.
    Originally I wrote a long explanation of how this worked as a comment,
    but the explanation was bigger than the file it was going into, so that
    got removed.  I'll post the bigger explanation on Facebook.

--- bash_history-rasppleii.sh
+++ bash_history-rasppleii.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 # vim: set ft=sh tabstop=4 shiftwidth=4 noexpandtab :
 
-[[  0 ]]
+# [[  0 ]] # Modify history (no longer needed)
 wget -O /home/pi/.bash_history ivanx.com/rasppleii/bash_history-rasppleii.txt
 history -c
 history -r
 
 
-[[  1 ]]
+# [[  1 ]] # Set system not to stat X11 by default
 sudo systemctl set-default multi-user.target &> /dev/null
 sudo shutdown -r now
 
 
-[[  2 ]]
+# [[  2 ]] # Fix NOOBS's twiddling with video settings
 if ! grep -q NOOBS /etc/rc.local; then
 	sudo sed -i "s@^exit 0@# Remove NOOBS's video config.txt video tampering.  The Pi's bootloader is
 # unable to detect some nonstandard HDMI displays, so unless the user tells
@@ -36,7 +36,7 @@ if grep -q NOOBS /boot/config.txt; then
 fi
 
 
-[[  3 ]]
+# [[  3 ]] # Configure keyboard, timezone, and locale
 sudo rm /etc/default/keyboard
 sudo debconf-set-selections <<EOF
 keyboard-configuration keyboard-configuration/modelcode select pc104
@@ -65,6 +65,7 @@ EOF
 sudo dpkg-reconfigure -f noninteractive locales
 source /etc/default/locale
 
+# disable running raspi-config at reboot if enabled
 if [[ -e /etc/profile.d/raspi-config.sh ]]; then
 	sudo rm -f /etc/profile.d/raspi-config.sh
 	sudo sed -i /etc/inittab -e "s/^#\(.*\)#\s*RPICFG_TO_ENABLE\s*/\1/" -e "/#\s*RPICFG_TO_DISABLE/d"
@@ -72,31 +73,31 @@ if [[ -e /etc/profile.d/raspi-config.sh ]]; then
 fi
 
 
-[[  4 ]]
+# [[  4 ]] # Install A2SERVER
 cd /tmp
 wget -O setup ivanx.com/a2server/setup
 source setup -y -w
 
 
-[[  5 ]]
+# [[  5 ]] # Install A2CLOUD
 cd /tmp
 rm /usr/local/adtpro/disks/A2CLOUD.* 2> /dev/null
 wget -O setup ivanx.com/a2cloud/setup
 source setup -y -r
 
 
-[[  6 ]]
+# [[  6 ]] # Get version number and write out /etc/issue
 sudo wget -O /etc/issue ivanx.com/rasppleii/issue-rasppleii.txt
 echo -n "Raspple II release number? "
 read
 sudo sed -i "s/Raspple II release.*$/Raspple II release $REPLY/" /etc/issue
 
 
-[[  7 ]]
+# [[  7 ]] # Install MOTD
 sudo wget -O /etc/motd ivanx.com/rasppleii/motd-rasppleii.txt
 
 
-[[  8 ]]
+# [[  8 ]] # build BOOT.TAR on sda1 from /boot
 mkdir -p /tmp/usbdisk
 sudo mount /dev/sda1 /tmp/usbdisk
 sudo rm /tmp/usbdisk/BOOT.TAR &> /dev/null
@@ -108,7 +109,7 @@ sudo umount /dev/sda1
 rmdir /tmp/usbdisk
 
 
-[[  9 ]]
+# [[  9 ]] # build ROOT.TAR on sda1 from /
 mkdir -p /tmp/usbdisk
 sudo mount /dev/sda1 /tmp/usbdisk
 sudo rm /tmp/usbdisk/ROOT.TAR &> /dev/null
--- bash_history-vm.sh
+++ bash_history-vm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # vim: set ft=sh tabstop=4 shiftwidth=4 noexpandtab :
 
-[[  0 ]]
+# [[  0 ]] # Modify history (no longer needed)
 wget -O /home/user1/.bash_history ivanx.com/rasppleii/bash_history-vm.txt
 history -c
 history -r
@@ -13,25 +13,28 @@ sudo tee /etc/sudoers.d/group-sudo-nopasswd >/dev/null <<EOF
 EOF
 sudo chmod 440 /etc/sudoers.d/group-sudo-nopasswd
 
-[[  2 ]]
-sudo sed -i '0,/-eq 0/s/-eq 0/-ge 0/' /etc/profile
+# [[  2 ]] # Use default root path instead of user path for all users
+# sed to English: Only in the range of line 0 to the first line containing
+# pattern /-eq 0/, replace that pattern with "-ge 0".
+sudo sed -i '0,/-eq 0/{ s/-eq 0/-ge 0/ }' /etc/profile
 source /etc/profile
 
-[[  3 ]]
-cd /tmp; wget -O setup ivanx.com/a2server/setup
+# [[  3 ]] # Install A2SERVER
+cd /tmp
+wget -O setup ivanx.com/a2server/setup
 source setup -y -w
 
-[[  4 ]]
+# [[  4 ]] # Install A2CLOUD
 cd /tmp
 rm /usr/local/adtpro/disks/A2CLOUD.* 2> /dev/null
 wget -O setup ivanx.com/a2cloud/setup
 source setup -y -r
 
-[[  5 ]]
+# [[  5 ]] # Get version number and write out /etc/issue
 sudo wget -O /etc/issue ivanx.com/rasppleii/issue-vm.txt
 echo -n "A2SERVER VM release number? "
 read
 sudo sed -i "s/A2SERVER VM release.*$/A2SERVER VM release $REPLY/" /etc/issue
 
-[[  6 ]]
+# [[  6 ]] # Install MOTD
 sudo wget -O /etc/motd ivanx.com/rasppleii/motd-vm.txt

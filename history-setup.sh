d40a0fdac5acc1213c9c36306a0f93d11654552c
T. Joseph Carter <tjcarter@spiritsubstance.com>
2016-09-05 08:16:57 -0700

    history-setup.sh: Just nuke it
    Y'know on second thought, we don't need that script at all.  The history
    files themselves show the history -c and history -r commands, so there's
    no need for this even as a method to teach people about them.  Plus,
    we're going to be dispensing with the hsitory files as history files
    anyway in the near future.

--- history-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# vim: set filetype=sh tabstop=4 shiftwidth=4 expandtab :
-
-if [[ -f /usr/bin/raspi-config ]]; then
-    wget -O /home/pi/.bash_history ivanx.com/rasppleii/bash_history-rasppleii.txt; history -c; history -r
-else
-    wget -O /home/user1/.bash_history ivanx.com/rasppleii/bash_history-vm.txt; history -c; history -r
-fi
